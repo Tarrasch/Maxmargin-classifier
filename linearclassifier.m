427d7f530389056b5df755a686ed17f0fd29e99f
Arash Rouhani <miffoljud@gmail.com>
2011-02-19 15:53:26 +0100

    Added testdata file.

--- linearclassifier.m
+++ linearclassifier.m
@@ -1,8 +1,9 @@
 %{
  TBD:       w and b
- Given:     set D = {xi, yi} (the training data),
+ Given:     (multi)set D = {xi, yi} (the training data), (n elements)
             where xi is a vector in d-dimensional space
             and  yi is ±1 depending in group it belongs in
             
+            We simply represent that multiset as X[n][d] and Y[n] 
 }%
 
