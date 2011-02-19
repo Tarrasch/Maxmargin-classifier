f7ad4350a116fdc8efae42b09afe03e88bf91ea8
Arash Rouhani <miffoljud@gmail.com>
2011-02-19 15:34:23 +0100

    Added short description for linear classifier

---
+++ linearclassifier.m
@@ -0,0 +1,8 @@
+%{
+ TBD:       w and b
+ Given:     set D = {xi, yi} (the training data),
+            where xi is a vector in d-dimensional space
+            and  yi is ±1 depending in group it belongs in
+            
+}%
+
